8a4f27c92ca5ad802ac5d23fd9327b9fec2f574a
sippnex <julian.sobotka@gmail.com>
2019-11-24 17:43:03 +0100

    quick fix

--- hx711-test.js
+++ hx711-test.js
@@ -2,9 +2,11 @@ var HX711 = require('hx711');
 
 const clockPin = 3;
 const dataPin = 2;
-
-console.log('hx711 test');
-
 const sensor = new HX711(clockPin, dataPin);
+
 //sensor.tare();
-console.log(sensor.getUnits());
+
+const getWeight = () => {
+    console.log(sensor.getUnits());
+    setTimeout(getWeight, 1000);
+};
