5d2dcb889b88325a523136ebe0b715ee707dac4c
sippnex <julian.sobotka@gmail.com>
2019-11-24 17:38:44 +0100

    added hx711 test js file

--- scales-cube.js
+++ scales-cube.js
@@ -3,14 +3,8 @@ var HX711 = require('hx711');
 const clockPin = 3;
 const dataPin = 2;
 
+console.log('hx711 test');
+
 const sensor = new HX711(clockPin, dataPin);
-
-module.exports.getWeight = () => {
-  console.log('scalesCube.getWeight()');
-  return sensor.getUnits();
-}
-
-module.exports.tare = () => {
-  console.log('scalesCube.tare()');
-  sensor.tare();
-}
+//sensor.tare();
+console.log(sensor.getUnits());
