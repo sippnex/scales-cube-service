44d5b0f4d82129ec2950092d99440327fc0c1afc
sippnex <julian.sobotka@gmail.com>
2019-11-24 14:39:45 +0100

    fixed characteristic namings

--- tare-characteristic.js
+++ tare-characteristic.js
@@ -4,7 +4,7 @@ var scalesCube = require('./scales-cube');
 
 var BlenoCharacteristic = bleno.Characteristic;
 
-var TareCharacteristics = function() {
+var TareCharacteristic = function() {
     TareCharacteristic.super_.call(this, {
         uuid: '17900496-ff17-4920-be38-b3686a9c7ce4',
         properties: ['write', 'notify'],
@@ -17,34 +17,34 @@ var TareCharacteristics = function() {
 
 util.inherits(TareCharacteristic, BlenoCharacteristic);
 
-/*TareCharacteristics.prototype.onReadRequest = function(offset, callback) {
-    console.log('TareCharacteristics - onReadRequest: value = ' + this._value.toString('hex'));
+/*TareCharacteristic.prototype.onReadRequest = function(offset, callback) {
+    console.log('TareCharacteristic - onReadRequest: value = ' + this._value.toString('hex'));
 
     callback(this.RESULT_SUCCESS, this._value);
 };*/
 
-TareCharacteristics.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+TareCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
     this._value = data;
 
-    console.log('TareCharacteristics - onWriteRequest: value = ' + this._value.toString('hex'));
+    console.log('TareCharacteristic - onWriteRequest: value = ' + this._value.toString('hex'));
     scalesCube.tare();
 
     if (this._updateValueCallback) {
-        console.log('TareCharacteristics - onWriteRequest: notifying');
+        console.log('TareCharacteristic - onWriteRequest: notifying');
         this._updateValueCallback(1);
     }
 
     callback(this.RESULT_SUCCESS);
 };
 
-TareCharacteristics.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
-    console.log('TareCharacteristics - onSubscribe');
+TareCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+    console.log('TareCharacteristic - onSubscribe');
     this._updateValueCallback = updateValueCallback;
 };
 
-TareCharacteristics.prototype.onUnsubscribe = function() {
-    console.log('TareCharacteristics - onUnsubscribe');
+TareCharacteristic.prototype.onUnsubscribe = function() {
+    console.log('TareCharacteristic - onUnsubscribe');
     this._updateValueCallback = null;
 };
 
-module.exports = TareCharacteristics;
+module.exports = TareCharacteristic;
--- weight-characteristic.js
+++ weight-characteristic.js
@@ -4,8 +4,8 @@ var scalesCube = require('./scales-cube');
 
 var BlenoCharacteristic = bleno.Characteristic;
 
-var WeightCharacteristics = function() {
-    WeightCharacteristics.super_.call(this, {
+var WeightCharacteristic = function() {
+    WeightCharacteristic.super_.call(this, {
         uuid: 'de5098d0-e052-400b-9482-6468cbfeb74c',
         properties: ['read', 'notify'],
         value: 0
@@ -15,21 +15,21 @@ var WeightCharacteristics = function() {
     this._updateValueCallback = null;
 };
 
-util.inherits(WeightCharacteristics, BlenoCharacteristic);
+util.inherits(WeightCharacteristic, BlenoCharacteristic);
 
-WeightCharacteristics.prototype.onReadRequest = function(offset, callback) {
-    console.log('WeightCharacteristics - onReadRequest: value = ' + this._value.toString('hex'));
+WeightCharacteristic.prototype.onReadRequest = function(offset, callback) {
+    console.log('WeightCharacteristic - onReadRequest: value = ' + this._value.toString('hex'));
     scalesCube.getWeight();
     callback(this.RESULT_SUCCESS, this._value);
 };
 
-/*WeightCharacteristics.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+/*WeightCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
     this._value = data;
 
-    console.log('WeightCharacteristics - onWriteRequest: value = ' + this._value.toString('hex'));
+    console.log('WeightCharacteristic - onWriteRequest: value = ' + this._value.toString('hex'));
 
     if (this._updateValueCallback) {
-        console.log('WeightCharacteristics - onWriteRequest: notifying');
+        console.log('WeightCharacteristic - onWriteRequest: notifying');
 
         this._updateValueCallback(this._value);
     }
@@ -37,14 +37,14 @@ WeightCharacteristics.prototype.onReadRequest = function(offset, callback) {
     callback(this.RESULT_SUCCESS);
 };*/
 
-WeightCharacteristics.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
-    console.log('WeightCharacteristics - onSubscribe');
+WeightCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+    console.log('WeightCharacteristic - onSubscribe');
     this._updateValueCallback = updateValueCallback;
 };
 
-WeightCharacteristics.prototype.onUnsubscribe = function() {
-    console.log('WeightCharacteristics - onUnsubscribe');
+WeightCharacteristic.prototype.onUnsubscribe = function() {
+    console.log('WeightCharacteristic - onUnsubscribe');
     this._updateValueCallback = null;
 };
 
-module.exports = WeightCharacteristics;
+module.exports = WeightCharacteristic;
